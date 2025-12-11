1fdfdfaa9973d147f270ca20613da3c2e3074054
prajain321 <prajain@amd.com>
2025-12-11 10:51:30 +0000

    Refactor cron_update.py to improve job function clarity and update scheduling frequency; added schedule to requirements.txt.

--- cron_update.py
+++ cron_update.py
@@ -1,16 +1,24 @@
-import schedule
 import time
+
+import schedule
+
 from packages.database.schema import Database
 from packages.balancer import force_refetch_and_update
 from common import get_rocm_unique_value
 
 db = Database('7.2')
-def job():
-    arr = db.get_all_collections()
-    for i in arr:
-        force_refetch_and_update(rocm_version=i, unique_key=str(get_rocm_unique_value(i)))
 
-# Run every 10 minutes
+
+def job():
+    """Execute scheduled job to refetch and update all collections."""
+    arr = db.get_all_collections()
+    print("Refetching and updating all collections...")
+    for i in arr:
+        force_refetch_and_update(
+            rocm_version=i, unique_key=str(get_rocm_unique_value(i)))
+
+
+# Run every second
 schedule.every(30).minutes.do(job)
 
 while True:
